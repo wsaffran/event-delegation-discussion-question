500fb50a15372e9309a8fea6b6badfbcd2374859
github username <github email address>
2019-04-05 10:11:08 -0400

    first commit

--- index.js
+++ index.js
@@ -1,7 +1,29 @@
 document.addEventListener('DOMContentLoaded', () => {
   const catContainer = document.getElementById('cat-container')
+  const catHeader = document.getElementById('cat-header')
 
   catContainer.innerHTML = renderCats()
+
+  catHeader.addEventListener('click', function(e) {
+    if(e.target.className === 'ui blue header center aligned') {
+      e.target.className = 'ui purple header center aligned'
+    }
+    else {
+      e.target.className = 'ui blue header center aligned'
+    }
+  })
+
+  catContainer.addEventListener('click', function(e) {
+    if (e.target.className === 'ui header') {
+      e.target.className = 'ui header green'
+    } else if (e.target.className === 'ui header green') {
+      e.target.className = 'ui header'
+    } if (e.target.className === 'thumbs up icon') {
+      let dataId = e.target.dataset.id
+      let likes = document.getElementById(`cat-${dataId}`).innerText
+      document.getElementById(`cat-${dataId}`).innerText = (parseInt(likes) + 1).toString()
+    }
+  })
 
 })
 
